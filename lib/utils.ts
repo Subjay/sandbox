74a7253afacf326884503175ce9b109a4196d9cb
Sébastien Gillig <115191795+Subjay@users.noreply.github.com>
2023-07-17 16:07:59 +0200

    adding console.log to see what's happening

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,9 @@ export default function getURL(path: string) {
   const baseURL = process.env.VERCEL_URL === undefined
     ? process.env.NEXT_PUBLIC_SITE_URL!
     : process.env.VERCEL_URL;
+    console.log("path",path);
+    console.log("baseURL",baseURL);
+    console.log("res",new URL(path, baseURL).toString());
   return new URL(path, baseURL).toString();
 }
 
