013673eb7a88c4d718addc31383c134be9fefa6c
Sébastien Gillig <115191795+Subjay@users.noreply.github.com>
2023-07-17 16:18:09 +0200

    Fix getURL issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,12 +3,5 @@ export default function getURL(path: string) {
     ? process.env.NEXT_PUBLIC_SITE_URL!
     : process.env.VERCEL_URL;
     
-    console.log("path",path);
-    console.log("baseURL",baseURL);
-    console.log("res", "https://" + baseURL + path);
   return ("https://" + baseURL + path).toString();
-}
-
-export function isConnected(): boolean{
-  return true;
 }
