6b848884aeb78e28e9d358c88850cce137d527a4
Sébastien Gillig <115191795+Subjay@users.noreply.github.com>
2023-07-17 16:14:13 +0200

    Try n°2 to fix getURL

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,9 +2,10 @@ export default function getURL(path: string) {
   const baseURL = process.env.VERCEL_URL === undefined
     ? process.env.NEXT_PUBLIC_SITE_URL!
     : process.env.VERCEL_URL;
+    
     console.log("path",path);
     console.log("baseURL",baseURL);
-    console.log("res", baseURL + path);
+    console.log("res", "https://" + baseURL + path);
   return (baseURL + path).toString();
 }
 
